2b50d63edf705c3f853eee3aa89b564c58bfcf40
Vinicius <vfarias.designer@gmail.com>
2026-07-21 22:00:44 -0300

    create styles css configs

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/Theme.js';
+import { GlobalStyles } from './styles/GlobalStyles.js';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
---
+++ src/styles/Theme.tsx
@@ -0,0 +1,31 @@
+
+export type Theme = {
+    color: {
+        Card: string,
+        Border: string,
+        Primary: string,
+        Foreground: string,
+        Background: string,
+        Primary_Foreground: string,
+        Muted: string,
+        Accent: string,
+        Destructive: string,
+        Primary_Hover: string
+    }
+};
+
+export const theme: Theme = {
+    color: {
+        Card: '#14181F',
+        Border: '#272C35',
+        Primary: '#07B6D5',
+        Foreground: '#F9FAFB',
+        Background: '#0B0E13',
+        Primary_Foreground: '#0B0E13',
+        Muted: '#2B303B',
+        Accent: '#F5993D',
+        Destructive: '#EF4343',
+        Primary_Hover: '#09778B'
+    }
+};
+
